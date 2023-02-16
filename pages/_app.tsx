a85759fa2b5b6a1023d87c609989699550a7ac5d
lrondon-prog <feliperondon123@hotmail.com>
2023-02-16 15:55:50 -0300

    First commit

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import Header from 'components/Header';
+import type { AppProps } from "next/app";
+import "styles/globals.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
+  );
+}
